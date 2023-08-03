8e7eb33e65645f6399123662c8ab513b3d2339a6
cje <cje960215@naver.com>
2023-08-03 12:50:13 +0900

    네번째 커밋

--- src/main/webapp/resources/js/mypageContentList.js
+++ src/main/webapp/resources/js/mypageContentList.js
@@ -7,8 +7,8 @@ document.getElementById("btn-submit").addEventListener("click", function(){
     $.ajax({
 
         url : "member/mypage/contentList",
-                /* 게시글 내용, 제목으로 검색하는데 내가 쓴 게시글이어야 해서 세가지 가져옴 */
-        data : {"boardContent" : inputSearch.value, "boardTitle" : inputSearch.value, "memberNo" : memberNo},
+                /* 게시글 제목으로 검색하는데 내가 쓴 게시글이어야 해서 세가지 가져옴 */
+        data : {"boardCode" : boardCode, "boardTitle" : inputSearch.value, "memberNo" : memberNo},
         type : "POST",
         dataType : "JSON",
 
